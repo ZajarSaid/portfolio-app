48de93f4bf7f0e04d15b2984b57ce253b5abb46d
Zakaria Said <zajarjafary@gmail.com>
2026-09-21 18:32:46 +0300

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,9 @@ function Footer() {
     <footer className="footer">
       <div className="footer__inner">
         <p>© {new Date().getFullYear()} Zakaria Saidi</p>
-        <p className="footer__tagline">
-          Built with JavaScript &amp; Python
-        </p>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
